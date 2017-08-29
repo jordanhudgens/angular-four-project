e097bea2746aa13a53c1a075d7fc5a5df005ce30
Jordan Hudgens <jordan@wow.com>
2017-08-29 09:58:19 -0700

    Built out the side component and model

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Cookies', 4),
+    new Ingredient('Steak', 42),
+    new Ingredient('Water', 1)
+  ];
 
   constructor() { }
 
