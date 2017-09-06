6503f999411cb7467d4513f73eabf03f49568fbd
Jordan Hudgens <jordan@wow.com>
2017-09-06 11:57:01 -0700

    Implemented the ability to access related data models through various components

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,31 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'Desc path', 'http://hackingdistributed.com/images/2014-11-23-macaroons-in-hyperdex/pretty-macaroons.jpg'),
-    new Recipe('Another Recipe', 'A longer description for the recipe', 'http://hackingdistributed.com/images/2014-11-23-macaroons-in-hyperdex/pretty-macaroons.jpg')
+    new Recipe(
+      'A Test Recipe',
+      'Desc path',
+      'http://hackingdistributed.com/images/2014-11-23-macaroons-in-hyperdex/pretty-macaroons.jpg',
+      [
+        new Ingredient('Apples', 55),
+        new Ingredient('Pears', 55),
+        new Ingredient('Bananas', 55)
+      ]
+    ),
+    new Recipe(
+      'Another Recipe',
+      'A longer description for the recipe',
+      'http://hackingdistributed.com/images/2014-11-23-macaroons-in-hyperdex/pretty-macaroons.jpg',
+      [
+        new Ingredient('Apples', 55),
+        new Ingredient('Pears', 55),
+        new Ingredient('Bananas', 55)
+      ]
+    )
   ];
 
   getRecipes() {
